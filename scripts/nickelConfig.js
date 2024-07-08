ad1efabbcb9b7971c982b9a0a5cfcfd6bef3c27a
acestronautical <acestronautical@protonmail.com>
2024-07-07 19:08:15 -0700

    format config js

--- scripts/nickelConfig.js
+++ scripts/nickelConfig.js
@@ -1,78 +1,138 @@
 const NickelConfigOptions = {
     label: "config",
-    title: "Configure menu label, icon, add or remove menu items",
+    title: "Configure menu label, icon, and add or remove menu items",
     options: {
-        menu_main_15505_label: [{ label: "label", title: "sets the label used for the NickelMenu button", placeholder: "Shortcuts" }],
+        menu_main_15505_label: [
+            { 
+                label: "label", 
+                title: "Set the label used for the NickelMenu button", 
+                placeholder: "Shortcuts" 
+            }
+        ],
         "experimental:menu_main_15505_icon": [
             {
                 label: "icon",
-                title: "sets icon used for the NickelMenu button",
+                title: "Set the icon used for the NickelMenu button",
                 options: {
                     star: "assets/star.png",
                     bookmark: "assets/bookmark.png",
-                    custom: [{ label: "upload", title: "upload an icon", file: "assets/.nickel.png" }]
+                    custom: [
+                        { 
+                            label: "upload", 
+                            title: "Upload an icon", 
+                            file: "assets/.nickel.png" 
+                        }
+                    ]
                 }
-            },
+            }
         ],
-        'experimental:menu_main_15505_icon_active':  [
+        'experimental:menu_main_15505_icon_active': [
             {
                 label: "active_icon",
-                title: "sets icon used for the NickelMenu button",
+                title: "Set the icon used for the active NickelMenu button",
                 options: {
                     star: "assets/star.png",
                     bookmark: "assets/bookmark.png",
-                    custom: [{ label: "upload", title: "upload an icon", file: "assets/.nickel_active.png" }]
+                    custom: [
+                        { 
+                            label: "upload", 
+                            title: "Upload an active icon", 
+                            file: "assets/.nickel_active.png" 
+                        }
+                    ]
                 }
-            },
+            }
         ],
         menu_item: [
             {
                 label: "location",
-                title: "the menu to add the item to",
+                title: "Menu to add the item to",
                 options: {
-                    main: "the menu added to the tabs on the bottom-right",
-                    reader: "the overflow menu in the reader",
-                    browser: "the menu in the bottom-right of the web browser",
-                    library: "the menu in the filter bar for the My Books and My Articles library views",
-                    selection: "the selection menu",
-                    selection_search: "the search sub-menu of the selection menu",
+                    main: "Menu added to the tabs on the bottom-right",
+                    reader: "Overflow menu in the reader",
+                    browser: "Menu in the bottom-right of the web browser",
+                    library: "Menu in the filter bar for the My Books and My Articles library views",
+                    selection: "Selection menu",
+                    selection_search: "Search sub-menu of the selection menu",
                 }
             },
             {
                 label: "label",
-                title: "the label to show on the menu item (must not contain :)",
-                placeholder: "menu item name"
+                title: "Label to show on the menu item (must not contain :)",
+                placeholder: "Menu item name"
             },
             {
                 label: "actions",
-                title: "the type of action to run",
+                title: "Type of action to run",
                 options: {
-                    cmd_spawn: [{ label: "cmd", title: "starts a command in the background", placeholder: "logread > /mnt/onboard/.adds/syslog.log" }],
-                    cmd_output: [{ label: "cmd", title: "runs a command, waits for it to exit, and optionally displays the output", placeholder: ":500:quiet :/usr/bin/pkill -f \"^/usr/bin/tcpsvd -E 0.0.0.0 2023\"" }],
-                    dbg_syslog: [{ label: "message", title: "writes a message to syslog (for testing)", placeholder: "text to write" }],
-                    dbg_error: [{ label: "message", title: "always returns an error (for testing)", placeholder: "the error message" }],
-                    dbg_msg: [{ label: "message", title: "shows a message (for testing)", placeholder: "the message" }],
-                    dbg_toast: [{ label: "message", title: "shows a toast (for testing)", placeholder: "the message" }],
-                    kfmon: [{ label: "filename", title: "triggers a kfmon action", placeholder: "the filename of the KFMon watched item to launch" }],
+                    cmd_spawn: [
+                        { 
+                            label: "cmd", 
+                            title: "Start a command in the background", 
+                            placeholder: "logread > /mnt/onboard/.adds/syslog.log" 
+                        }
+                    ],
+                    cmd_output: [
+                        { 
+                            label: "cmd", 
+                            title: "Run a command, wait for it to exit, and optionally display the output", 
+                            placeholder: ":500:quiet :/usr/bin/pkill -f \"^/usr/bin/tcpsvd -E 0.0.0.0 2023\"" 
+                        }
+                    ],
+                    dbg_syslog: [
+                        { 
+                            label: "message", 
+                            title: "Write a message to syslog (for testing)", 
+                            placeholder: "Text to write" 
+                        }
+                    ],
+                    dbg_error: [
+                        { 
+                            label: "message", 
+                            title: "Always return an error (for testing)", 
+                            placeholder: "Error message" 
+                        }
+                    ],
+                    dbg_msg: [
+                        { 
+                            label: "message", 
+                            title: "Show a message (for testing)", 
+                            placeholder: "Message" 
+                        }
+                    ],
+                    dbg_toast: [
+                        { 
+                            label: "message", 
+                            title: "Show a toast (for testing)", 
+                            placeholder: "Message" 
+                        }
+                    ],
+                    kfmon: [
+                        { 
+                            label: "filename", 
+                            title: "Trigger a KFMon action", 
+                            placeholder: "Filename of the KFMon watched item to launch" 
+                        }
+                    ],
                     nickel_setting: [
                         {
                             label: "action",
-                            title: "how to change the setting",
+                            title: "How to change the setting",
                             options: {
-                                toggle: "toggles between true/false",
-                                enable: "sets to true",
-                                disable: "sets to false",
+                                toggle: "Toggle between true/false",
+                                enable: "Set to true",
+                                disable: "Set to false",
                             }
                         },
                         {
                             label: "setting",
-                            title: "the setting to change",
+                            title: "Setting to change",
                             options: {
-                                invert: "FeatureSettings.InvertScreen (this requires a reboot to apply on 4.28.18220+, and may not work on newer devices)",
+                                invert: "FeatureSettings.InvertScreen (requires reboot to apply on 4.28.18220+, may not work on newer devices)",
                                 dark_mode: "ReadingSettings.DarkMode",
                                 lockscreen: "PowerSettings.UnlockEnabled",
                                 screenshots: "FeatureSettings.Screenshots",
-                                force_wifi: "DeveloperSettings.ForceWifiOn (note: the setting doesn't apply until you toggle WiFi)",
+                                force_wifi: "DeveloperSettings.ForceWifiOn (setting doesn't apply until you toggle WiFi)",
                                 auto_usb_gadget: "Automatically enable USB mass storage on connection",
                             }
                         }
@@ -80,7 +140,7 @@ const NickelConfigOptions = {
                     nickel_extras: [
                         {
                             label: "app",
-                            title: "open a nickel application",
+                            title: "Open a Nickel application",
                             options: {
                                 unblock_it: "unblock_it",
                                 sketch_pad: "sketch_pad",
@@ -90,73 +150,95 @@ const NickelConfigOptions = {
                             }
                         }
                     ],
-                    nickel_browser: [{ label: "url", title: "Open the web browser", placeholder: "url to open as homepage" }],
-                    "nickel_browser:modal": [{ label: "url", title: "Open the web browser as a pop-up", placeholder: "url to open as homepage" }],
-                    nickel_misc: [{
-                        label: "misc",
-                        title: " various useful actions",
-                        options: {
-                            home: "goes to the home screen",
-                            force_usb_connection: "forces a usb connection dialog to be shown",
-                            rescan_books: "forces nickel to rescan books",
-                            rescan_books_full: "forces a full usb connect/disconnect cycle",
+                    nickel_browser: [
+                        { 
+                            label: "url", 
+                            title: "Open the web browser", 
+                            placeholder: "URL to open as homepage" 
                         }
-                    }],
-                    nickel_open: [{
-                        label: "open",
-                        title: "open a kobo menu",
-                        options: {
-                            "discover:storefront": "Kobo Store",
-                            "discover:wishlist": "Wishlist",
-                            "library:library": "My Books (with last tab and filter)",
-                            "library:all": "Books",
-                            "library:authors": "Authors",
-                            "library:series": "Series (4.20.14601+)",
-                            "library:shelves": "Collections",
-                            "library:pocket": "Articles",
-                            "library:dropbox": "Dropbox",
-                            "reading_life:reading_life": "Activity (with last tab)",
-                            "reading_life:stats": "Activity",
-                            "reading_life:awards": "Awards",
-                            "reading_life:words": "My Words",
-                            "store:overdrive": "OverDrive",
-                            "store:search": "Search",
+                    ],
+                    "nickel_browser:modal": [
+                        { 
+                            label: "url", 
+                            title: "Open the web browser as a pop-up", 
+                            placeholder: "URL to open as homepage" 
                         }
-                    }],
-                    nickel_wifi: [{
-                        label: "wifi setting",
-                        title: "modify wifi behavior",
-                        options: {
-                            autoconnect: "attempts to enable and connect to wifi (similar to what happens when you open a link)",
-                            autoconnect_silent: "attempts to connect to wifi in the background (does nothing if wifi is disabled, the battery is low, is already connected, or there aren't any known networks in range) (no errors are shown) (similar to what happens when you turn on the Kobo)",
-                            enable: "enables WiFi (but doesn't necessarily connect to it)",
-                            disable: "disables WiFi",
-                            toggle: "toggles WiFi (but doesn't necessarily connect to it)",
+                    ],
+                    nickel_misc: [
+                        {
+                            label: "misc",
+                            title: "Various useful actions",
+                            options: {
+                                home: "Go to the home screen",
+                                force_usb_connection: "Force a USB connection dialog to be shown",
+                                rescan_books: "Force Nickel to rescan books",
+                                rescan_books_full: "Force a full USB connect/disconnect cycle",
+                            }
                         }
-                    }],
-                    nickel_orientation: [{
-                        label: "orientation",
-                        title: "change screen orientation",
-                        options: {
-                            portrait: "portrait mode",
-                            landscape: "landscape mode",
-                            inverted_portrait: "inverted_portrait mode",
-                            inverted_landscape: "inverted_landscape mode",
-                            invert: "invert mode",
-                            swap: "swap mode",
+                    ],
+                    nickel_open: [
+                        {
+                            label: "open",
+                            title: "Open a Kobo menu",
+                            options: {
+                                "discover:storefront": "Kobo Store",
+                                "discover:wishlist": "Wishlist",
+                                "library:library": "My Books (with last tab and filter)",
+                                "library:all": "Books",
+                                "library:authors": "Authors",
+                                "library:series": "Series (4.20.14601+)",
+                                "library:shelves": "Collections",
+                                "library:pocket": "Articles",
+                                "library:dropbox": "Dropbox",
+                                "reading_life:reading_life": "Activity (with last tab)",
+                                "reading_life:stats": "Activity",
+                                "reading_life:awards": "Awards",
+                                "reading_life:words": "My Words",
+                                "store:overdrive": "OverDrive",
+                                "store:search": "Search",
+                            }
                         }
-                    }],
-                    power: [{
-                        label: "power action",
-                        title: "perform a power action",
-                        options: {
-                            shutdown: "shutdown",
-                            reboot: "reboot",
-                            sleep: "sleep",
+                    ],
+                    nickel_wifi: [
+                        {
+                            label: "wifi setting",
+                            title: "Modify WiFi behavior",
+                            options: {
+                                autoconnect: "Attempt to enable and connect to WiFi (similar to when opening a link)",
+                                autoconnect_silent: "Attempt to connect to WiFi in the background (does nothing if WiFi is disabled, battery is low, already connected, or no known networks in range) (no errors shown) (similar to when turning on the Kobo)",
+                                enable: "Enable WiFi (but not necessarily connect to it)",
+                                disable: "Disable WiFi",
+                                toggle: "Toggle WiFi (but not necessarily connect to it)",
+                            }
                         }
-                    }],
+                    ],
+                    nickel_orientation: [
+                        {
+                            label: "orientation",
+                            title: "Change screen orientation",
+                            options: {
+                                portrait: "Portrait mode",
+                                landscape: "Landscape mode",
+                                inverted_portrait: "Inverted portrait mode",
+                                inverted_landscape: "Inverted landscape mode",
+                                invert: "Invert mode",
+                                swap: "Swap mode",
+                            }
+                        }
+                    ],
+                    power: [
+                        {
+                            label: "power action",
+                            title: "Perform a power action",
+                            options: {
+                                shutdown: "Shutdown",
+                                reboot: "Reboot",
+                                sleep: "Sleep",
+                            }
+                        }
+                    ],
                 }
             }
         ]
     }
-}
+};
